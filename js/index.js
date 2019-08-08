69454a6aec659648777ed93d2b0a1e4aa4389769
Danny Town <47186976+dannyotown@users.noreply.github.com>
2019-08-07 19:14:57 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -40,3 +40,7 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+let navBar = document.querySelectorAll('nav');
+navBar.forEach(element => {
+  
+});
